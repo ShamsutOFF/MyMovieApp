3b4fcfb5aad81d228f74ba4d9840b6c23f7c16c1
ShamsutOFF <shamsutoff@yandex.ru>
2021-07-26 11:16:22 +0300

    Перевел но Котлин

--- app/src/main/java/com/example/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/example/mymovieapp/MainActivity.kt
@@ -1,26 +1,16 @@
-package com.example.mymovieapp;
+package com.example.mymovieapp
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-    private Button button;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, button.getText(), Toast.LENGTH_SHORT).show();
-            }
-        }
-        );
+class MainActivity : AppCompatActivity() {
+    private var button: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        button?.setOnClickListener { Toast.makeText(this, button?.text, Toast.LENGTH_SHORT).show() }
     }
 }
